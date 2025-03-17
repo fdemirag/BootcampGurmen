895b1aa9536bf8ab9aed97e1f2f1d9f251018935
fdemirag <f2000fatih@hotmail.com>
2025-03-17 09:38:44 +0300

    İlk controller yazıldı.

---
+++ Bootcamp.API/Controllers/ExampleController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bootcamp.API.Controllers
+{
+    [Route("api/[controller]/[action]")]  
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok("Merhaba Dünya[GET]");
+        }
+        // S  s
+
+        [HttpPost]
+        public IActionResult SaveProduct()
+        {
+            return Ok("Merhaba Dünya[Post]");
+        }
+        [HttpPut]
+        public IActionResult UpdateProduct()
+        {
+            return Ok("Merhaba Dünya[Put]");
+        }
+
+
+        [HttpDelete]
+        public IActionResult DeleteProduct()
+        {
+            return Ok("Merhaba Dünya[Delete]");
+        }
+    }
+}
